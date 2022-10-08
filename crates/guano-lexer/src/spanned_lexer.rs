0594043ad3e5967bce334a713a0a67ad99fdcabe
Noah Shanaberger <noah@thenoah.party>
2022-10-08 00:28:11 -0400

    Got a working lexer
    Spans may be iffy, but so far, so good. Let me sleep

---
+++ crates/guano-lexer/src/spanned_lexer.rs
@@ -0,0 +1,249 @@
+use std::ops::RangeInclusive;
+
+use logos::{Lexer, Logos, Source, Span as ByteSpan};
+
+use crate::token::Token;
+
+/*
+*   This is probably, most definitely, shitty.
+*   And to you I say: I'm tired, leave me alone, I just need it to work.
+*   Also I did this when my brain has been more or less nonfunctional, so uh yeah, imma go to sleep.
+*
+*   - Noah Shanaberger, October 8th 2022 - 12:27 AM.
+*/
+
+#[derive(Debug, Clone)]
+pub struct Span {
+    pub byte_span: ByteSpan,
+    pub line_span: LineSpan,
+}
+
+#[derive(Debug, Clone)]
+pub struct LineSpan {
+    pub lines: RangeInclusive<usize>,
+    pub start_line_column: usize,
+    pub end_line_column: usize,
+}
+
+#[derive(Debug, Clone)]
+pub struct LineSegment<'source, S: ?Sized> {
+    pub number: usize,
+    pub byte_span: ByteSpan,
+    pub value: &'source S,
+}
+
+pub struct SpannedLexer<'source, Token: Logos<'source>> {
+    pub lexer: Lexer<'source, Token>,
+    pub newlines: Vec<Option<usize>>,
+}
+
+impl<'source, Token: Logos<'source>> SpannedLexer<'source, Token> {
+    pub fn new(lexer: Lexer<'source, Token>) -> Self {
+        Self {
+            lexer,
+            newlines: vec![None],
+        }
+    }
+
+    pub fn slice(&self) -> &<Token::Source as Source>::Slice {
+        self.lexer.slice()
+    }
+
+    pub fn extras(&self) -> &Token::Extras {
+        &self.lexer.extras
+    }
+
+    pub fn extras_mut(&mut self) -> &mut Token::Extras {
+        &mut self.lexer.extras
+    }
+
+    pub fn get_line(&self, number: usize) -> Option<&<Token::Source as Source>::Slice> {
+        let newlines_index = number.checked_sub(1)?;
+
+        let start = self.newlines.get(newlines_index)?.map_or(0, |i| i + 1);
+        let end = self
+            .newlines
+            .get(newlines_index + 1)
+            .map_or(self.lexer.source().len(), |n| n.unwrap());
+
+        self.lexer.source().slice(start..end)
+    }
+
+    pub fn line_start(&self, number: usize) -> Option<usize> {
+        self.newlines
+            .get(number.checked_sub(1)?)?
+            .map_or(Some(0), |s| Some(s + 1))
+    }
+
+    pub fn line_end(&self, number: usize) -> Option<usize> {
+        self.newlines
+            .get(number)
+            .map_or(Some(self.lexer.span().end), |s| *s)
+    }
+
+    pub fn lines(&self) -> Option<Vec<(usize, &<Token::Source as Source>::Slice)>> {
+        let mut lines = vec![];
+        let mut windows = self.newlines.windows(2).map(|w| w.try_into().unwrap());
+
+        while let Some::<[Option<usize>; 2]>([first, second]) = windows.next() {
+            let start = first.map_or(0, |s| s + 1);
+            let end = second?;
+
+            lines.push((lines.len() + 1, self.lexer.source().slice(start..end)?))
+        }
+
+        let start = self.newlines.last().cloned().flatten().map_or(0, |s| s + 1);
+
+        let end = self.lexer.source().len();
+
+        lines.push((lines.len() + 1, self.lexer.source().slice(start..end)?));
+
+        Some(lines)
+    }
+
+    pub fn line_segments(
+        &self,
+        span: &LineSpan,
+    ) -> Option<Vec<LineSegment<'source, <Token::Source as Source>::Slice>>> {
+        let mut line_iter = span.lines.clone();
+
+        let mut lines = Vec::from([{
+            let number = line_iter.next()?;
+            let start_index = self.line_start(number)? + span.start_line_column;
+            let end_index = self.line_end(number)?;
+
+            let byte_span = start_index..end_index;
+
+            let value = self.lexer.source().slice(byte_span.clone())?;
+            LineSegment {
+                number,
+                byte_span,
+                value,
+            }
+        }]);
+
+        while let Some(number) = line_iter.next() {
+            let start_index = self.line_start(number)?;
+            let end_index = self.line_end(number)?;
+
+            let byte_span = start_index..end_index;
+
+            let value = self.lexer.source().slice(byte_span.clone())?;
+            lines.push(LineSegment {
+                number,
+                byte_span,
+                value,
+            })
+        }
+
+        let segment = lines.last_mut()?;
+        let end = self.line_start(segment.number)? + span.end_line_column;
+
+        segment.byte_span.end = end;
+        segment.value = self.lexer.source().slice(segment.byte_span.clone())?;
+
+        Some(lines)
+    }
+
+    pub fn line_number(&self) -> usize {
+        self.newlines.len()
+    }
+}
+
+impl<'source, Token: Logos<'source> + Clone> Clone for SpannedLexer<'source, Token>
+where
+    Token::Extras: Clone,
+{
+    fn clone(&self) -> Self {
+        Self::new(self.lexer.clone())
+    }
+}
+
+impl<'source, Token: Logos<'source> + std::fmt::Debug> std::fmt::Debug
+    for SpannedLexer<'source, Token>
+where
+    Token::Extras: std::fmt::Debug,
+    Token::Source: std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("SpannedLexer")
+            .field("lexer", &self.lexer)
+            .field("newlines", &self.newlines)
+            .finish()
+    }
+}
+
+impl<'source, Token: Logos<'source> + PartialEq + PushNewlines<'source>> Iterator
+    for SpannedLexer<'source, Token>
+{
+    type Item = (Token, Span);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let token = self.lexer.next()?;
+            let start_line = self.line_number();
+            let start_location = self.newlines.last().unwrap().map_or(0, |u| u + 1);
+
+            token.push_newlines(&self.lexer, &mut self.newlines);
+
+            if token != Token::NEWLINE {
+                let byte_span = self.lexer.span();
+
+                let end_line = self.line_number();
+                let end_location = self.newlines.last().unwrap().map_or(0, |u| u + 1);
+
+                let span = Span {
+                    line_span: LineSpan {
+                        lines: start_line..=end_line,
+                        start_line_column: byte_span.start - start_location,
+                        end_line_column: byte_span.end - end_location,
+                    },
+
+                    byte_span,
+                };
+
+                return Some((token, span));
+            }
+        }
+    }
+}
+
+pub trait ToSpanned<'source, Token: Logos<'source>> {
+    fn to_spanned(self) -> SpannedLexer<'source, Token>;
+}
+
+impl<'source, Token: Logos<'source>> ToSpanned<'source, Token> for Lexer<'source, Token> {
+    fn to_spanned(self) -> SpannedLexer<'source, Token> {
+        SpannedLexer::new(self)
+    }
+}
+
+pub trait PushNewlines<'source>: Logos<'source> + PartialEq {
+    const NEWLINE: Self;
+    fn push_newlines(&self, lex: &Lexer<'source, Self>, newlines: &mut Vec<Option<usize>>)
+        -> usize;
+}
+
+impl<'source> PushNewlines<'source> for Token {
+    const NEWLINE: Self = Self::Newline;
+    fn push_newlines(
+        &self,
+        lex: &Lexer<'source, Token>,
+        newlines: &mut Vec<Option<usize>>,
+    ) -> usize {
+        match self {
+            Self::Newline => {
+                newlines.push(Some(lex.span().start));
+                1
+            }
+            Self::CommMulti((_, n)) => {
+                for newline in n {
+                    newlines.push(Some(*newline));
+                }
+
+                n.len()
+            }
+            _ => 0,
+        }
+    }
+}
--- crates/guano-lexer/src/token.rs
+++ crates/guano-lexer/src/token.rs
@@ -1,125 +1,354 @@
+use logos::{Lexer, Logos};
+
+/// Represents a lexical token.
+#[derive(Debug, Clone, PartialEq, Logos)]
+#[logos(subpattern decimal = r"[0-9][_0-9]*")]
+pub enum Token {
+    // Keywords
+    /// Function definition
+    #[token("fun")]
+    KeyFun,
+
+    /// Constant definition
+    #[token("let")]
+    KeyLet,
+
+    /// Variable definition
+    #[token("var")]
+    KeyVar,
+
+    /// Return keyword
+    #[token("ret")]
+    KeyRet,
+
+    /// Type cast keyword
+    #[token("as")]
+    KeyAs,
+
+    // Primitive Types
+    /// String type
+    #[token("str")]
+    PrimStr,
+
+    /// Char type
+    #[token("char")]
+    PrimChar,
+
+    /// Unsigned integer type
+    #[regex(r"u(\d+|size)", bitsize)]
+    PrimUnsignedInteger(BitSize),
+
+    /// Signed integer type
+    #[regex(r"i(\d+|size)", bitsize)]
+    PrimInteger(BitSize),
+
+    /// Floating point type
+    #[regex(r"f(\d+|size)", bitsize)]
+    PrimFloat(BitSize),
+
+    // Literals
+    // The in-memory value of these literals will not
+    // be parsed in the lexer. That is for the parser.
+    // However, there is an exception: character and string literals.
+    /// Signed or unsigned integer literal
+    #[regex(r"(?&decimal)", store_string)]
+    LitInteger(String),
+
+    /// Floating point literal
+    #[regex(r"(?&decimal)\.(?&decimal)", store_string)]
+    LitFloat(String),
+
+    /// String literal
+    #[regex(r#""(?:[^"\\\n]|\\.)*""#, text_literal)]
+    LitString(String),
+
+    /// Char literal
+    #[regex(r"'(?:[^'\\\n]|\\.)*'", text_literal)]
+    LitChar(String),
+
+    /// Represents a hexadecimal value.
+    /// What exact type is held will be infered base upon the context within the parser.
+    #[regex(r"0[xX][0-9A-Fa-f][_0-9A-Fa-f]*", remove_first_two)]
+    LitHex(String),
+
+    /// Represents a binary value.
+    /// What exact type is infered by the parser.
+    #[regex(r"0[bB][01][_01]*", remove_first_two)]
+    LitBin(String),
+
+    // General
+    /// Whitespace
+    #[regex(r"\s", logos::skip)]
+    Whitespace,
+
+    /// Identifier
+    #[regex(r"[A-Za-z_][0-9A-Za-z_]*", store_string)]
+    Identifier(String),
+
+    // Comments
+    /// Single line comment
+    #[regex(r"##[^\n]*", remove_first_two)]
+    CommSingle(String),
+
+    /// Multi line comment
+    #[regex(r"#\$", multiline_comment)]
+    CommMulti((String, Vec<usize>)),
+
+    // Single Character Tokens
+    /// {
+    #[token("{")]
+    OpenBrace,
+
+    /// Newline
+    #[token("\n")]
+    Newline,
+
+    /// }
+    #[token("}")]
+    CloseBrace,
+
+    /// [
+    #[token("[")]
+    OpenBracket,
+
+    /// ]
+    #[token("]")]
+    CloseBracket,
+
+    /// (
+    #[token("(")]
+    OpenParen,
+
+    /// )
+    #[token(")")]
+    CloseParen,
+
+    /// =
+    #[token("=")]
+    Equals,
+
+    /// &
+    #[token("&")]
+    Ampersand,
+
+    /// @
+    #[token("@")]
+    Asperand,
+
+    /// ,
+    #[token(",")]
+    Comma,
+
+    /// :
+    #[token(":")]
+    Colon,
+
+    /// ;
+    #[token(";")]
+    Semicolon,
+
+    /// %
+    #[token("%")]
+    Percent,
+
+    /// ^
+    #[token("^")]
+    Caret,
+
+    /// !
+    #[token("!")]
+    Exclamation,
+
+    /// |
+    #[token("|")]
+    Pipe,
+
+    /// ~
+    #[token("~")]
+    Tilda,
+
+    /// +
+    #[token("+")]
+    Plus,
+
+    /// -
+    #[token("-")]
+    Minus,
+
+    /// /
+    #[token("/")]
+    Slash,
+
+    /// *
+    #[token("*")]
+    Asterisk,
+
+    /// <
+    #[token("<")]
+    LessThan,
+
+    /// >
+    #[token(">")]
+    GreaterThan,
+
+    /// .
+    #[token(".")]
+    Period,
+
+    /// Error
+    #[error]
+    Error,
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum BitSize {
     SystemSize,
     Specific(u8), // 8, 16, 32, and 64 are the exclusive valid options.
 }
 
-impl ToString for BitSize {
-    fn to_string(&self) -> String {
+impl std::fmt::Display for BitSize {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::SystemSize => "size".into(),
-            Self::Specific(bit_size) => format!("{}", bit_size),
+            Self::SystemSize => f.write_str("size"),
+            Self::Specific(bit_size) => write!(f, "{}", bit_size),
         }
     }
 }
 
-/// Represents a lexical token.
-#[derive(Debug, Clone, PartialEq)]
-pub enum Token {
-    /// EOF
-    EOF,
+fn bitsize(lex: &mut Lexer<Token>) -> Option<BitSize> {
+    Some(match &lex.slice()[1..] {
+        "size" => BitSize::SystemSize,
+        "8" => BitSize::Specific(8),
+        "16" => BitSize::Specific(16),
+        "32" => BitSize::Specific(32),
+        "64" => BitSize::Specific(64),
+        _ => return None,
+    })
+}
 
-    // Keywords
-    /// Function definition
-    KeyFun,
-    /// Constant definition
-    KeyLet,
-    /// Variable definition
-    KeyVar,
-    /// Return keyword
-    KeyRet,
-    /// Type cast keyword
-    KeyAs,
+fn store_string(lex: &mut Lexer<Token>) -> String {
+    lex.slice().to_owned()
+}
 
-    // Primitive Types
-    /// String type
-    PrimStr,
-    /// Char type
-    PrimChar,
-    /// Unsigned integer type
-    PrimUnsignedInteger(BitSize),
-    /// Signed integer type
-    PrimInteger(BitSize),
-    /// Floating point type
-    PrimFloat(BitSize),
+fn text_literal(lex: &mut Lexer<Token>) -> Option<String> {
+    let end = lex.slice().len() - 1;
+    lex.slice().get(1..end).map(|s| s.to_owned())
+}
 
-    // Literals
-    // The in-memory value of these literals will not
-    // be parsed in the lexer. That is for the parser.
-    /// Signed or unsigned integer literal
-    LitInteger(String),
-    /// Floating point literal
-    LitFloat(String),
-    /// String literal
-    LitString(String),
-    /// Char literal
-    LitChar(String),
-    /// Represents a hexadecimal value.
-    /// What exact type is held will be infered base upon the context within the parser.
-    LitHex(String),
-    /// Represents a binary value.
-    /// What exact type is infered by the parser.
-    LitBin(String),
+fn remove_first_two(lex: &mut Lexer<Token>) -> Option<String> {
+    lex.slice().get(2..).map(|s| s.to_owned())
+}
 
-    // General
-    /// Whitespace
-    Whitespace,
-    /// Identifier
-    Identifier(String),
+fn multiline_comment(lex: &mut Lexer<Token>) -> Option<(String, Vec<usize>)> {
+    let mut last_character = '$';
+    let mut closed = false;
 
-    // Comments
-    /// Single line comment
-    CommSingle(String),
-    /// Multi line comment
-    CommMulti(String),
+    let mut newline_indexes = vec![];
 
-    // Single Character Tokens
-    /// {
-    OpenBrace,
-    /// }
-    CloseBrace,
-    /// [
-    OpenBracket,
-    /// ]
-    CloseBracket,
-    /// (
-    OpenParen,
-    /// )
-    CloseParen,
-    /// =
-    Equ,
-    /// &       
-    Amp,
-    /// @
-    Asp,
-    /// ,
-    Com,
-    /// :
-    Col,
-    /// ;
-    Sem,
-    /// %
-    Per,
-    /// ^
-    Car,
-    /// !
-    Exc,
-    /// |
-    Pip,
-    /// ~
-    Til,
-    /// +
-    Plu,
-    /// -
-    Min,
-    /// /
-    Sla,
-    /// *
-    Ast,
-    /// <
-    Les,
-    /// >
-    Gre,
-    /// .
-    Dot,
+    for (index, character) in lex.remainder().chars().enumerate() {
+        if character == '\n' {
+            newline_indexes.push(lex.span().end);
+        }
+
+        lex.bump(character.len_utf8());
+
+        if character == '#' && last_character == '$' && index != 0 {
+            closed = true;
+            break;
+        }
+
+        last_character = character;
+    }
+
+    if closed {
+        let end = lex.slice().len() - 2;
+
+        lex.slice()
+            .get(2..end)
+            .map(|s| (s.to_owned(), newline_indexes))
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use logos::Logos;
+
+    use crate::spanned_lexer::ToSpanned;
+
+    use super::Token;
+
+    #[test]
+    fn test_text() {
+        let string = r#""Hello, world! \r\n \t \x41 \u0041 \U00000041"
+        
+        
+        "Hello, world! \r\n \t \x41 \u0041 \U00000041" 
+        ""#;
+        let character = r#"'H' '"#;
+
+        let mut string_lexer = Token::lexer(string).to_spanned();
+
+        while let Some((token, _)) = string_lexer.next() {
+            if let Token::Error = token {
+                println!("Error causing string: {:#?}", string_lexer.slice());
+            } else {
+                println!("{:#?}", token);
+            }
+        }
+
+        println!("{:#?}", string_lexer.lines());
+
+        println!("------");
+
+        let mut character_lexer = Token::lexer(character);
+
+        while let Some(token) = character_lexer.next() {
+            if let Token::Error = token {
+                println!("Error causing string: {:#?}", character_lexer.slice());
+            } else {
+                println!("{:#?}", token);
+            }
+        }
+    }
+
+    #[test]
+    fn test_comment() {
+        let single = "## Hello, world!\n _ignore ## Other Comment";
+        let multi = "#$ Rando!\r\n\n\n\n\nPPPEEEE  \n$$\n$$$$# \n\n#$$#\n\n###";
+
+        let mut single_lexer = Token::lexer(single).to_spanned();
+
+        while let Some((token, span)) = single_lexer.next() {
+            dbg!(single_lexer.line_segments(&span.line_span));
+
+            if let Token::Error = token {
+                println!("Error causing string: {:#?}", single_lexer.slice());
+            } else {
+                println!("{:#?}", token);
+            }
+        }
+
+        println!("------");
+
+        let mut multi_lexer = Token::lexer(multi).to_spanned();
+
+        while let Some((token, span)) = multi_lexer.next() {
+            dbg!(multi_lexer.line_segments(&span.line_span));
+
+            match token {
+                Token::Error => {
+                    println!("Error causing string: {:#?}", multi_lexer.slice());
+                }
+                Token::CommMulti((s, _)) => {
+                    println!("Comment Contents: {:#?}", s);
+                }
+                _ => (),
+            }
+        }
+        
+
+        println!("{:#?}", multi_lexer.lines());
+    }
 }
